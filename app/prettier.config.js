0d4caf2f16725cd32fb8b5ec518869a681966588
Owais Jamil <desertthunder.dev@gmail.com>
2026-07-14 17:01:08 -0500

    build: scaffold web app

---
+++ app/prettier.config.js
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+const config = {
+	objectWrap: 'collapse',
+	bracketSameLine: true,
+	useTabs: true,
+	singleQuote: true,
+	trailingComma: 'none',
+	printWidth: 120,
+	plugins: ['prettier-plugin-svelte'],
+	overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }]
+};
+
+export default config;
